f41e6d597511b37a9e7f6ef05f38fa847d4239fa
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-06-30 10:04:17 +0530

    callback hell

---
+++ index3.js
@@ -0,0 +1,28 @@
+// asynchronous
+console.log("Before");
+getUser(1, (user) => {
+  getUser(1, (user) => {
+    getRepositories(user.gitHubUsername, (repos) => {
+      getCommits(repo, (commits) => {
+        //   CALLBACK HELL
+      });
+    });
+  });
+});
+
+console.log("After");
+
+function getUser(id, callback) {
+  setTimeout(() => {
+    console.log("Reading a user from database...");
+
+    callback({ id: id, gitHubUsername: "Nilank Nikhil" });
+  }, 2000);
+}
+
+function getRepositories(username, callback) {
+  setTimeout(() => {
+    console.log("Calling Github API...");
+    callback(["repo1", "repo2", "repo3"]);
+  }, 2000);
+}
