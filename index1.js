ca41d3fd96ad2c1bc739fd24b2af835f76a6a7e7
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-06-30 09:48:09 +0530

    patterns for dealing with asynchronous code

---
+++ index1.js
@@ -0,0 +1,12 @@
+console.log('Before');
+const user = getUser(1);
+console.log(user);
+console.log('After');
+// there are 3 patterns to deal with aynchronous code: callbacks, prmoise, async/await
+function getUser(id){
+    setTimeout(() => {
+        console.log('Reading a user from database...');
+        return { id: id, gitHubUsername: 'Nilank Nikhil'};
+    },2000);
+    return 1;
+}
