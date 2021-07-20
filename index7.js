183a704bb8747d7c1836de4120ab89223f591eb0
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-07-20 21:53:18 +0530

    done

--- index7.js
+++ index7.js
@@ -2,5 +2,6 @@ const promise = Promise.resolve({ id: 1 });
 promise.then((result) => console.log(result));
 
 
+
 const promise1 = Promise.reject(new Error('Reason for rejection...'));
 promise1.catch((error) => console.log(error));
