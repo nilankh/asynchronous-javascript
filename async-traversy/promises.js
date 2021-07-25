a558cb8525c50a527f97dbd87053078405f81f08
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-07-25 12:29:55 +0530

    async await part1

--- async-traversy/promises.js
+++ async-traversy/promises.js
@@ -37,15 +37,32 @@ function createPost(post) {
 //   .then(getPosts)
 //   .catch(err => console.log(err))
 
-const promise1 = Promise.resolve('Hello World');
-const promise2 = 10;
-const promise3 = new Promise((resolve, reject) =>
-  setTimeout(resolve, 2000, 'Goodbye')
-);
-const promise4 = fetch('https://jsonplaceholder.typicode.com/users').then(
-  (res) => res.json()
-);
+// Async demo
+async function init(){
+  await createPost({
+      title: 'Post Three',
+      body: 'This is post three',
+    });
+    getPosts();
+}
+init();
 
-Promise.all([promise1, promise2, promise3, promise4]).then((values) =>
-  console.log(values)
-);
+
+
+
+
+
+
+// const promise1 = Promise.resolve('Hello World');
+// const promise2 = 10;
+// const promise3 = new Promise((resolve, reject) =>
+//   setTimeout(resolve, 2000, 'Goodbye')
+// );
+// const promise4 = fetch('https://jsonplaceholder.typicode.com/users').then(
+//   (res) => res.json()
+// );
+
+
+// Promise.all([promise1, promise2, promise3, promise4]).then((values) =>
+//   console.log(values)
+// );
